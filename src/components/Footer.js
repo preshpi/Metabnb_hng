bd054ef0258b19eced9ece362cd295abedd2bc4c
preshpi <egwuenuprecious7@gamil.com>
2022-11-15 12:40:52 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,70 +4,64 @@ import { GrFacebookOption } from 'react-icons/gr';
 import { AiOutlineTwitter } from 'react-icons/ai';
 function Footer() {
   return (
-    <div className="lg:h-[322px] bg-[#1D1D1E] text-[#F7F7F7]">
-      <div className="mx-5 justify-center items-center place-items-center">
-         <div className="grid lg:grid-cols-2  lg:mx-[113px]">
-            <div className="w-[420px]">
-              <div className="mt-14">
-                  <img src="https://res.cloudinary.com/dpokiomqq/image/upload/v1668273027/footer_dxtqdp.svg" alt="footer logo"/>
+      <div className="bg-[#1D1D1E] lg:h-[322px] md:h-[700px] h-[1150px]">
+        <div className="lg:grid-cols-4 md:grid-cols-2 grid lg:mx-[113px] md:place-items-center md:items-center md:mx-11">
+          <div className="mt-[62px] lg:place-items-start md:text-start text-center grid place-items-center ">
+              <img src="https://res.cloudinary.com/dpokiomqq/image/upload/v1668273027/footer_dxtqdp.svg" alt="logo"/>
+
+            <div className="lg:block md:block hidden ">
+              <div className="mt-[106px] flex gap-12 text-[#F7F7F7]">
+                <GrFacebookOption/>
+                <BsInstagram/>
+                <AiOutlineTwitter/>
               </div>
 
-              <div className="hidden lg:block">
-              <div className="flex mt-20 gap-5 ">
-                <GrFacebookOption className="w-[18px] text-[#F7F7F7] h-[18px]"/>
-                <BsInstagram className="w-[18px] text-[#F7F7F7] h-[18px]"/>
-                <AiOutlineTwitter className="w-[18px] text-[#F7F7F7] h-[18px]"/>
-              </div>
-
-              <div className="mt-12">
-                <p className="font-[16px] leading-[16px]"><span className="mr-2">©</span>2022 Metabnb</p>
-              </div>
-              </div>           
+              <div className="mt-12 mb-[37px] font-[16px] leading-[16px] text-[#F7F7F7]">
+                <span> © 2022 Metabnb</span>
+              </div>             
             </div>
-
-             <div className="lg:mt-10 mt-20 mb-20 lg:justify-around lg:gap-[80px]  gap-[10px] gap-70 lg:mr-[209px] mr-[99px] justify-center place-items-center">
-               <div className="lg:grid-cols-3 md:grid-cols-2 gap-8 grid justify-center place-items-center items-center">
-                  <div className="lg:h-24 justify-center items-center place-items-center ">
-                    <h3 className="font-bold text-lg leading-5 text-[#FFFFFF] lg:text-start text-center">Community</h3>
-                    <p className="mt-8 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center">NFTs</p>
-                    <p className="mt-2 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center">Tokens</p>
-                    <p className="mt-2 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center">Landlords</p>
-                    <p className="mt-2 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center mb-5">Discord</p>
-                  </div>
-                  <div className="lg:h-24">
-                    <h3 className="font-bold text-lg leading-5 text-[#FFFFFF] lg:text-start text-center">Places</h3>
-                    <p className="mt-8 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center">Castle</p>
-                    <p className="mt-2 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center">Farm</p>
-                    <p className="mt-2 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center">Beach</p>
-                    <p className="mt-2 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center mb-5">Learn more</p>
-                  </div>
-                  <div className="lg:h-24">
-                    <h3 className="font-bold text-lg leading-5 text-[#FFFFFF] lg:text-start text-center">About us</h3>
-                    <p className="mt-8 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center">Road map</p>
-                    <p className="mt-2 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center">Creators</p>
-                    <p className="mt-2 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center">Career</p>
-                    <p className="mt-2 text-[#F1F3F9] text-sm leading-8 font-normal lg:text-start text-center mb-3">Contact us</p>
-                  </div>
-                </div>
-             </div> 
-
-
-              <div className="justify-center items-center place-items-center lg:hidden md:hidden">
-               <div className="flex mt-20 text-center gap-8">
-                <GrFacebookOption className="w-[18px] text-[#F7F7F7] h-[18px]"/>
-                <BsInstagram className="w-[18px] text-[#F7F7F7] h-[18px]"/>
-                <AiOutlineTwitter className="w-[18px] text-[#F7F7F7] h-[18px]"/>
-               </div>
-
-              <div className="text-center flex mt-12 mb-8">
-                <p className="font-[16px] leading-[16px] text-center"><span className="mr-2">©</span>2022 Metabnb</p>
-              </div>
-              </div>
  
-         </div> 
-      </div>
+          </div>
 
-    </div>
+          <div className="mt-[63px] grid place-items-center">
+            <h3 className="font-bold text-lg leading-5 text-[#FFFFFF]">Community</h3>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-6">NFT</p>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-4">Tokens</p>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-4">Landlords</p>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-4">Discords</p>
+          </div>
+
+          <div className="mt-[63px] grid place-items-center">
+            <h3 className="font-bold text-lg leading-5 text-[#FFFFFF]">Places</h3>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-6">Castle</p>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-4">Farms</p>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-4">Beach</p>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-4">Learn more</p>
+          </div>
+
+          <div className="mt-[63px] grid place-items-center">
+            <h3 className="font-bold text-lg leading-5 text-[#FFFFFF]">About us</h3>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-6">Road map</p>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-4">Creators</p>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-4">Career</p>
+            <p className="text-[#F1F3F9] text-sm leading-8 font-normal mt-4">Contact us</p>
+          </div>
+
+          <div className="lg:hidden md:hidden block grid place-items-center">
+         <div className="mt-[106px] flex gap-12 text-[#F7F7F7]">
+                <GrFacebookOption/>
+                <BsInstagram/>
+                <AiOutlineTwitter/>
+          </div>
+
+          <div className="mt-12 mb-[37px] font-[16px] leading-[16px] text-[#F7F7F7]">
+                <span> © 2022 Metabnb</span>
+          </div>            
+          </div>
+
+ 
+        </div>
+      </div>
   )
 }
 
